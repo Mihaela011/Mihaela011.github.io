4e23f69f1ad7165a21a9a3b2c462ef2b264d7a2d
Mihaela011 <44159922+Mihaela011@users.noreply.github.com>
2018-10-30 03:57:20 +1400

    corect

--- orientation.js
+++ orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.29.16";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.29.19";
 window.addEventListener("deviceorientation", "on_device_orientation");
 
 //-----------
@@ -8,4 +8,12 @@ function on_device_orientation(e)
 	documen.getElementById("id_beta").innerHTML=e.beta;
 	documen.getElementById("id_gamma").innerHTML=e.gamma;
 }
+
+var canvas= document.getElementById("id_canvas");
+var context=canvas.getContext("2d")
+
+
+context.beginPath();
+context.arc(canvas.width/2+e.gamma/90*(canvas.width/2-R), canvas.height/2+e.beta/90*(canvas.width/2-R),R,0,2*Math.PI);
+context.stroke();
 //--------------------

868745bc3889bf93cc761931e905a7b23994d2ca
Mihaela011 <44159922+Mihaela011@users.noreply.github.com>
2018-10-30 01:50:01 +1400

    worker

--- calcul_prime.js
+++ calcul_prime.js
@@ -1,13 +1,11 @@
-
-
-function is_prim(n);
+function is_prim(n)
 {
 	for (var i=2; i*i<=n; i++)
 		if ( n mod i==0)
 			return false;
-for (var i=1e9; i<1e10; i++)
-	if (is_prim(i))
-		postMessage(i);
+    for (var i=1e9; i<1e10; i++)
+	     if (is_prim(i))
+		    postMessage(i);
 	
 postMessage("gata");
 

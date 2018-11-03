0693877f00e69e6f123b47cd3f3a192fe3a91110
Mihaela011 <44159922+Mihaela011@users.noreply.github.com>
2018-11-04 01:38:22 +1400

    corect0

--- ec.js
+++ ec.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.15.2";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.15.3";
 
 function rezolva()
 {

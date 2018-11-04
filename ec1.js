4396a653879f091a1135ea474e8b3db27687b3c5
Mihaela011 <44159922+Mihaela011@users.noreply.github.com>
2018-11-05 08:18:19 +1400

    change0

--- ec1.js
+++ ec1.js
@@ -1,4 +1,4 @@
-document.getElementById("id_text").innerHTML="Logic version  2018.11.04.03";
+document.getElementById("id_text").innerHTML="Logic version  2018.11.04.04";
 
 //atasare butonului functia rezolva
 document.getElementById("buton").addEventListener("click",rezolvare);

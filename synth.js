aacfc4fb0725c5f731caaa92c3b5920c12ec3171
Mihaela011 <44159922+Mihaela011@users.noreply.github.com>
2018-12-11 03:19:38 +1400

    test2

--- synth.js
+++ synth.js
@@ -1,5 +1,6 @@
 document.getElementById("id_logic_version").innerHTML="Logic version 2018.12.10.0"
-document.getElementById("id_get_voices").addEventListener=("click", on_get_voices)
+document.getElementById("id_get_voices").addEventListener=("click", on_get_voices);
+document.getElementById("id_speak").addEventListener=("click", on_speak);
 
 
 var synth =  window.speechSynthesis;
@@ -11,4 +12,13 @@ function on_get_voices()
 	for ( var i=1; i<voices.length; i++)
 		document.getElementById("id_voices_list").innerHTML +=
 	        voices[i].lang + " " + voices[i].name + "<br>";
+}
+
+
+function on_speak
+{
+	var enunt = new SpeechSynthesisUtterance();
+	enunt.lang= "en-US";
+	enunt.text=document.getElementById("id_text").value;
+	synth.speak(enunt);
 }
